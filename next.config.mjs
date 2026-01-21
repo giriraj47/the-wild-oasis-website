4e35818afe3c82da2600ec8016d404daaf4535fd
Giriraj Joshi <levilightsg@gmail.com>
2026-01-21 16:06:19 +0530

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zmobglhjayylkvifiupi.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+  // output: "export",
 };
 
 export default nextConfig;
